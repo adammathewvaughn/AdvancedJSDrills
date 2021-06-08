e7f2194beda4f58a3994c47a2b044c00fafc821f
Adam Vaughn <adammathewvaughn>
2021-06-08 14:55:49 -0500

    I think this is right

---
+++ script.js
@@ -0,0 +1,39 @@
+// setName();
+// setName = (() => {
+// var name= "Adam";
+// console.log(name);
+// });
+
+// console.log("some random text");
+// let avg = findAvg(2 , 2);
+// console.log("some random text", avg);
+// function findAvg(a, b) {
+//     console.log("some random text");
+//     var answer = (a + b) / 2;
+//     return answer;
+// }
+
+// let fruitsarray = ["apples", "bananas", "grapes", "pears"];
+// //let favfruit;
+// function printFruits() {
+//     let favfruit = fruitsarray[2];
+//     console.log(fruitsarray[0]);
+    
+//     function printFavFruit() {
+//         let leastfav = fruitsarray[3];
+//         console.log(leastfav);
+//         console.log(favfruit);
+//     }
+// }
+
+// printFruits();
+//printFavFruit();
+
+// funcyTown();
+// function funcyTown(){
+//     console.log(`Hello ${"Adam"}`)
+// }
+let alertFunction = function () {
+    alert("Some text of my choosing");
+};
+alertFunction();
